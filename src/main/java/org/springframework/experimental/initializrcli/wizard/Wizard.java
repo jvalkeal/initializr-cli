cc0f7f9c0090a519f2521b255bee672255a730cb
Janne Valkealahti <janne.valkealahti@gmail.com>
2021-12-30 16:02:36 +0000

    Fix wrong language print

--- src/main/java/org/springframework/experimental/initializrcli/wizard/Wizard.java
+++ src/main/java/org/springframework/experimental/initializrcli/wizard/Wizard.java
@@ -70,7 +70,7 @@ public class Wizard {
 		writeSingleSelector("Project", projectType);
 
 		String languageType = runSingleSelector(languageSelectorItems, "Language");
-		writeSingleSelector("Language", projectType);
+		writeSingleSelector("language", languageType);
 
 		String bootVersion = runSingleSelector(bootSelectorItems, "Spring Boot");
 		writeSingleSelector("Spring Boot", bootVersion);
