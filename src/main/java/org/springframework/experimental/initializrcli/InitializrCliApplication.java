557014397a22d1515fab29d0f34e26a9755d2ac0
Janne Valkealahti <janne.valkealahti@gmail.com>
2022-01-26 20:50:17 +0000

    stash

--- src/main/java/org/springframework/experimental/initializrcli/InitializrCliApplication.java
+++ src/main/java/org/springframework/experimental/initializrcli/InitializrCliApplication.java
@@ -17,55 +17,11 @@ package org.springframework.experimental.initializrcli;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.experimental.initializrcli.client.model.ArtifactId;
-import org.springframework.experimental.initializrcli.client.model.BootVersion;
-import org.springframework.experimental.initializrcli.client.model.Dependencies;
-import org.springframework.experimental.initializrcli.client.model.Dependency;
-import org.springframework.experimental.initializrcli.client.model.DependencyCategory;
-import org.springframework.experimental.initializrcli.client.model.Description;
-import org.springframework.experimental.initializrcli.client.model.GroupId;
-import org.springframework.experimental.initializrcli.client.model.IdName;
-import org.springframework.experimental.initializrcli.client.model.JavaVersion;
-import org.springframework.experimental.initializrcli.client.model.JavaVersion.JavaVersionValues;
-import org.springframework.experimental.initializrcli.client.model.Language;
-import org.springframework.experimental.initializrcli.client.model.Language.LanguageValues;
-import org.springframework.experimental.initializrcli.client.model.Metadata;
-import org.springframework.experimental.initializrcli.client.model.Name;
-import org.springframework.experimental.initializrcli.client.model.PackageName;
-import org.springframework.experimental.initializrcli.client.model.Packaging;
-import org.springframework.experimental.initializrcli.client.model.Packaging.PackagingValues;
-import org.springframework.experimental.initializrcli.client.model.ProjectType;
-import org.springframework.experimental.initializrcli.client.model.ProjectType.ProjectTypeValue;
-import org.springframework.experimental.initializrcli.client.model.Version;
-import org.springframework.nativex.hint.NativeHint;
-import org.springframework.nativex.hint.ResourceHint;
-import org.springframework.nativex.hint.TypeAccess;
-import org.springframework.nativex.hint.TypeHint;
 
-// @TypeHint(types = {
-// 		ArtifactId.class, BootVersion.class, Dependencies.class, Dependency.class, DependencyCategory.class,
-// 		Description.class, GroupId.class, IdName.class, JavaVersion.class, JavaVersionValues.class, Language.class,
-// 		LanguageValues.class, Metadata.class, Name.class, PackageName.class, Packaging.class, PackagingValues.class,
-// 		ProjectType.class, ProjectTypeValue.class, Version.class
-// }, access = { TypeAccess.PUBLIC_CLASSES, TypeAccess.PUBLIC_CONSTRUCTORS, TypeAccess.PUBLIC_FIELDS,
-// 		TypeAccess.PUBLIC_METHODS, TypeAccess.DECLARED_CLASSES, TypeAccess.DECLARED_CONSTRUCTORS,
-// 		TypeAccess.DECLARED_FIELDS, TypeAccess.DECLARED_METHODS })
-// @ResourceHint(patterns = {
-// 	"completion/.*",
-// 	"template/.*.st",
-// 	"com/sun/jna/win32-x86-64/jnidispatch.dll"
-// })
 @SpringBootApplication
 public class InitializrCliApplication {
 
 	public static void main(String[] args) {
-		// System.setProperty("jna.debug_load", "true");
-		// System.setProperty("jna.debug_load.jna", "true");
-		// String xxx1 = System.getProperty("jna.debug_load");
-		// System.out.println("XXX1 " + xxx1);
-		// boolean xxx2 = Boolean.getBoolean("jna.debug_load");
-		// System.out.println("XXX2 " + xxx2);
-
 		SpringApplication.run(InitializrCliApplication.class, args);
 	}
 }
