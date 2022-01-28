17bf0ceb67e40ecd63e97526847a6433c3edf7d2
Janne Valkealahti <janne.valkealahti@gmail.com>
2022-01-28 08:55:48 +0000

    Tweak native hints for win
    - Add spesific hints for win jna things to get
      native shell working.
    - Pump spring-native 0.11.2
    - Pump graal 22.0.0.2

--- src/main/java/org/springframework/experimental/initializrcli/InitializrCliApplication.java
+++ src/main/java/org/springframework/experimental/initializrcli/InitializrCliApplication.java
@@ -17,37 +17,6 @@ package org.springframework.experimental.initializrcli;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.experimental.initializrcli.client.model.ArtifactId;
-import org.springframework.experimental.initializrcli.client.model.BootVersion;
-import org.springframework.experimental.initializrcli.client.model.Dependencies;
-import org.springframework.experimental.initializrcli.client.model.Dependency;
-import org.springframework.experimental.initializrcli.client.model.DependencyCategory;
-import org.springframework.experimental.initializrcli.client.model.Description;
-import org.springframework.experimental.initializrcli.client.model.GroupId;
-import org.springframework.experimental.initializrcli.client.model.IdName;
-import org.springframework.experimental.initializrcli.client.model.JavaVersion;
-import org.springframework.experimental.initializrcli.client.model.JavaVersion.JavaVersionValues;
-import org.springframework.experimental.initializrcli.client.model.Language;
-import org.springframework.experimental.initializrcli.client.model.Language.LanguageValues;
-import org.springframework.experimental.initializrcli.client.model.Metadata;
-import org.springframework.experimental.initializrcli.client.model.Name;
-import org.springframework.experimental.initializrcli.client.model.PackageName;
-import org.springframework.experimental.initializrcli.client.model.Packaging;
-import org.springframework.experimental.initializrcli.client.model.Packaging.PackagingValues;
-import org.springframework.experimental.initializrcli.client.model.ProjectType;
-import org.springframework.experimental.initializrcli.client.model.ProjectType.ProjectTypeValue;
-import org.springframework.experimental.initializrcli.client.model.Version;
-import org.springframework.nativex.hint.TypeAccess;
-import org.springframework.nativex.hint.TypeHint;
-
-@TypeHint(types = {
-		ArtifactId.class, BootVersion.class, Dependencies.class, Dependency.class, DependencyCategory.class,
-		Description.class, GroupId.class, IdName.class, JavaVersion.class, JavaVersionValues.class, Language.class,
-		LanguageValues.class, Metadata.class, Name.class, PackageName.class, Packaging.class, PackagingValues.class,
-		ProjectType.class, ProjectTypeValue.class, Version.class
-}, access = { TypeAccess.PUBLIC_CLASSES, TypeAccess.PUBLIC_CONSTRUCTORS, TypeAccess.PUBLIC_FIELDS,
-		TypeAccess.PUBLIC_METHODS, TypeAccess.DECLARED_CLASSES, TypeAccess.DECLARED_CONSTRUCTORS,
-		TypeAccess.DECLARED_FIELDS, TypeAccess.DECLARED_METHODS })
 @SpringBootApplication
 public class InitializrCliApplication {
 
